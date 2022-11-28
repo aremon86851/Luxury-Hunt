fdb8aa152e95d70c9bdc8f121e7994654d19591a
Abdur Rahman Emon <aremon86851@gmail.com>
2022-11-29 01:10:10 +0600

    admin user and seller loaded succesfully

--- src/shared/AllUserSeller/AllUserSeller.js
+++ src/shared/AllUserSeller/AllUserSeller.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { handleDeleteRoute } from '../../utilities/handleDeleteRoute';
 
 const AllUserSeller = ({ i, info, refetch }) => {
-    const { userName, userEmail, _id } = info
+    const { userName, userEmail, _id, role } = info
 
     const handleDeleteUser = id => {
         handleDeleteRoute('allSeller', refetch, id)
+        handleDeleteRoute('allUser', refetch, id)
     }
     return (
         <tr>
             <th>{i + 1}</th>
             <td>{userName}</td>
             <td>{userEmail}</td>
-            <td><button className='btn btn-sm' onClick={() => handleDeleteUser(_id)}>DELETE</button></td>
-            <td><button className='btn btn-sm'>unverified</button></td>
+            <td>
+                {
+                    role !== "Admin" && <button className='btn btn-sm' onClick={() => handleDeleteUser(_id)}>DELETE</button>
+                }
+            </td>
+            <td>{
+                role === "Seller" && <button className='btn btn-sm'>unverified</button>
+            }</td>
         </tr>
     );
 };
